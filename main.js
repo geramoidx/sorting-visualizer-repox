cf8600e5fe6f4acf4608b2f43c1b84edb2eafbba
Gerald Annan <geraldannan400@gmail.com>
2020-11-03 01:37:24 -0800

    Updated main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var slider = document.getElementById("myRange");
+    var generateNewArrayBtn = document.getElementById("newArray");
+    var bubbleSortBtn = document.getElementById("bubbleSort");
+    var heapSortBtn = document.getElementById("heapSort");
+    var mergeSortBtn = document.getElementById("mergeSort");
+    var quickSortBtn = document.getElementById("quickSort");
+}
