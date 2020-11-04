297456bac6c18bb9260690cf7ef3452b420b4204
Gerald Annan <geraldannan400@gmail.com>
2020-11-04 02:08:43 -0800

    Updated main.js

--- main.js
+++ main.js
@@ -5,14 +5,54 @@ window.onload = function() {
     var heapSortBtn = document.getElementById("heapSort");
     var mergeSortBtn = document.getElementById("mergeSort");
     var quickSortBtn = document.getElementById("quickSort");
+    var HbarArray = [];
+    var ShiftedBars = [];
+    var IsHeapSorting = false;
+    var IsBubbleSorted = false;
 
-    class CanvasObject {
+    class NavigatorObject {
         constructor() {
+            let navigator = document.getElementById("navigator");
 
+            this.context = navigator.getContext('2d');
+
+            this._width = 350;
+            //canvas.style.margin = "auto";
+            this._height = 250;
+
+            navigator.width = this._width;
+            navigator.height = this._height;
+            navigator.style.backgroundColor = "#fff";
+            navigator.style.opacity = 0.8;
+
+            // this.SortSpeed = 10;
+            // console.log("created navigator");
         }
 
-        clear() {}
+        clear() {
+            this.getCtx.clearRect(0, 0, this._width, this._height);
+        }
 
-        get getCtx() {}
+        get getCtx() {
+            return this.context;
+        }
     }
+
+    const navigator = new NavigatorObject();
+    var ctxnav = navigator.getCtx;
+
+    const UpdateNavigator = () => {
+        ctxnav.strokeStyle = "rgb(110, 48, 48)";
+        ctxnav.lineWidth = 2;
+        ctxnav.strokeRect(1, 1, 350 - 2, 250 - 2);
+
+        ctxnav.fillStyle = "rgb(110, 48, 48)";
+        ctxnav.fillRect(1, 1, 350 - 2, 30);
+
+        ctxnav.font = 15 + "px consolas";
+        ctxnav.fillStyle = "#fff";
+        ctxnav.fillText("Navigator", 1 + (348 * 0.39), 20);
+    }
+
+    UpdateNavigator();
 }
