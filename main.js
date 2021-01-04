0491cccfe380f207dec414b40b1ea682004ac737
Gerald Annan <geraldannan400@gmail.com>
2021-01-04 15:07:18 -0800

    Updated main.js file

--- main.js
+++ main.js
@@ -83,3 +83,240 @@ class CanvasObject {
     return this.context;
   }
 }
+
+const canvas = new CanvasObject();
+let minErr = 0.40;
+let maxErr = 0.45;
+var midpoint = canvas._width * 0.5;
+const ctx = canvas.getCtx;
+
+var minNum = 5;
+var maxNum = 200;
+var n = maxNum;
+const gap = 1;
+let min = 100;
+let max = 500;
+var minWidth = 3;
+var maxWidth = 60;
+var hbarwidith = minWidth;
+var fswap = false;
+var lswap = false;
+var GeneralSpeed = 500;
+
+class ComponentHBar {
+    constructor(x, y, width, height, index) {
+        this._index = index;
+        this._x = x;
+        this._y = y;
+        this._wd = width;
+        this.rs = 0.5; // this is the resize ration of the Hbar component
+        this._var = this._wd * this.rs;
+        this._hg = height;
+        this.isResized = true;
+        this.speed = GeneralSpeed;
+        this.color = "#00b";
+    }
+
+    get CurrentObj() {
+        return this;
+    }
+
+    display() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this._x, this._y, this._wd, this._hg);
+        ctx.font = this._wd * 0.30 + "px consolas";
+
+        ctx.fillStyle = "#ffb";
+        ctx.fillText(this._hg, this._x + (this._wd * 0.25), 80);
+
+        ctx.strokeStyle = "#ffb";
+        ctx.lineWidth = 1;
+        ctx.strokeRect(this._x, this._y, this._wd, this._hg);
+    }
+
+    misplaced() {
+        this.color = "red";
+        this.display();
+    }
+
+    mark() {
+        this.color = "green";
+        this.display();
+    }
+
+    unmark() {
+        this.color = "#00b";
+        this.display();
+    }
+
+    sortedbar() {
+        this.color = "rgb(110, 48, 48)";
+        this.display();
+    }
+
+    resize(template = true) {
+
+        if (!this.isResized && template) {
+
+            if (this._wd > this._var) {
+                this._x += this.rs * 0.5;
+                this._wd += -this.rs;
+            } else {
+                this.isResized = true;
+            }
+        } else if (!this.isResized && !template) {
+            if (this._wd < this._var * 4) {
+                this._x += -this.rs * 0.5;
+                this._wd += this.rs;
+            } else {
+                this.isResized = true;
+            }
+        }
+
+        this.display();
+    }
+
+    unsize(destinationX, template = true) {
+
+        const unshrink = setInterval(() => {
+
+                if (Math.floor(this._x) != Math.floor(destinationX) && template) {
+                    UpdateCanvasArea();
+
+                    this._x -= this.rs * 0.5;
+                    this._wd += this.rs;
+
+                    this.display();
+
+                } else if (!template && Math.ceil(this._x) != Math.ceil(destinationX)) {
+                    UpdateCanvasArea();
+
+                    this._x += this.rs * 0.5;
+                    this._wd += -this.rs;
+
+                    this.display();
+                } else {
+
+                    // UpdateCanvasArea();
+                    //console.log("2nd : " + this._x + " width fac : " + this._wd);
+                    // if (!template) {
+                    //     this._x -= 10;
+                    // }
+
+                    // this.display();
+
+                    clearInterval(unshrink);
+                }
+
+            },
+            CanvasObject.SortSpeed);
+    }
+
+    moveTo(destinationX, inter) {
+        //this.resize(true);
+
+        //let leftbox = destinationX + (this._var * 2 * (this.rs ** 2));
+
+        if (Math.ceil(this._x) == Math.floor(destinationX)) {
+            this._x = destinationX;
+
+            this.unmark();
+            UpdateCanvasArea();
+            fswap = true;
+            clearInterval(inter);
+            // console.log("Stop to");
+
+
+            //this._x += this.speed;
+            //this.unsize(destinationX, true);
+
+        } else {
+            //let nextX = this._x + this.speed;
+
+            //this._x += this.speed;
+            // console.log("thisX " + this._x + ", dest " + destinationX);
+
+            let nextX = this._x + this.speed;
+
+
+
+            //this._x -= this.speed;
+            if (nextX > destinationX) {
+                // console.log("nextX " + nextX + " " + destinationX);
+                let lp = Math.floor(nextX - destinationX);
+                // console.log("this x:" + this._x + ", dest: " + destinationX + ", nextX: " + nextX + ", lp: " + lp);
+                this._x = destinationX;
+                //this._x = Math.ceil(this._x);
+                // console.log(this._x + ",== " + destinationX + " speed " + this.speed);
+                // console.log(this._x + ", " + destinationX);
+                clearInterval(inter);
+                this.unmark();
+                UpdateCanvasArea();
+                fswap = true;
+
+            } else {
+                this._x += this.speed;
+            }
+        }
+
+
+        this.display();
+    }
+
+    moveFrom(destinationX, inter) {
+        //this.resize(false);
+
+        //let leftbox = destinationX - (this._var * 2 * (this.rs ** 2));
+
+        if (this.isResized) {
+
+            if (Math.ceil(this._x) == Math.floor(destinationX)) {
+                this._x = destinationX;
+                //console.log("lfbx : " + leftbox);
+                clearInterval(inter);
+                this.unmark();
+                UpdateCanvasArea();
+                lswap = true;
+                // console.log("Stop from");
+
+                //this._x -= this.speed;
+                //this.unsize(destinationX, false);
+
+            } else {
+                let nextX = this._x - this.speed;
+
+                //this._x -= this.speed;
+                if (nextX < destinationX) {
+                    // console.log("configurfation settings");
+                    let lp = Math.ceil(destinationX - nextX);
+                    // console.log("this x:" + this._x + ", dest: " + destinationX + ", nextX: " + nextX + ", lp: " + lp);
+                    this._x = destinationX;
+                    //this._x = Math.floor(this._x);
+                    // console.log(this._x + ",== " + destinationX);
+
+                    this.unmark();
+                    UpdateCanvasArea();
+                    lswap = true;
+                    clearInterval(inter);
+
+                } else {
+                    this._x -= this.speed;
+                }
+            }
+        }
+
+        this.display();
+    }
+}
+
+const UpdateCanvasArea = () => {
+    canvas.clear();
+
+    for (let i = 0; i < HbarArray.length; i++) {
+        HbarArray[i].display();
+    }
+
+    for (let i = 0; i < ShiftedBars.length; i++) {
+        ShiftedBars[i].display();
+    }
+}
